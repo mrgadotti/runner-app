18bf844e133aca9038f2ba9c81bdde965f7758e7
Marcelo Gadotti <eng.marcelogadotti@gmail.com>
2023-08-29 09:53:43 -0300

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import { Main } from './styles';
+
+const Home: React.FC = () => {
+  console.log('home');
+  return (
+    <>
+      <div className='App'>HelloWorld</div>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/pages/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Main = styled.div`
+  /* height: calc(100vh - 5.5rem); */
+  height: 92%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
